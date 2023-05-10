f4e972c488f5be539cce5d9491a0efd8a7622a10
premkumarsaini <jhandu.saini@dotsquares.com>
2023-05-10 19:31:41 +0530

    created footer

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,50 +1,111 @@
 import * as React from "react";
-
+import { svgIcons } from "../../svgIcon ";
 type footer = {
+ 
   footerData: any;
-
 };
 let FooterContent: any = "";
+let c_footerAllMenus: any;
+
+
 const Footer = (props: footer) => {
   // action toggle switcher-trigger
 
- 
- FooterContent = props?.footerData?.c_footerBottomContent;
-//  console.log(FooterContent,"footerData");
+  FooterContent = props?.footerData?.c_footerBottomContent;
+  c_footerAllMenus = props?.footerData.c_footerAllMenus;
+
+
+  console.log(props.footerData, "footerData");
+
+
+  
   return (
     <>
       <footer className="page-footer">
         <div className="container-full-width">
           <div className="footer content">
-            {/* Crawled Footer From Live Site */}
-              <div className="footer-logo">
-                {/* Site Logo */}
-                  <img
-                    width="200"
-                    height="200"
-                    alt="logo"
-                    src={
-                      props.footerData?.c_footerLogo?.url}
-                  />
-                  {/* Site Logo */}
-              </div>
-               {/* footer Content */}
-              <div className="em-client__colophon">
-                      {FooterContent?.map((data: any) => {
-                        return(
+            <div className="footer-pages-links">
+              <div className="footer-columns">
+                <div className="column column1">
+                  <div className="footer-logo">
+                    {/* Site Logo */}
+                    <img
+                      width="200"
+                      height="200"
+                      alt="logo"
+                      src={props.footerData?.c_footerLogo?.url}
+                    />
+                    {/* Site Logo */}
+                  </div>
+                </div>
+
+          {c_footerAllMenus && c_footerAllMenus.map((item: any, index: number) => {
+              return (
+                <>
+                <div className="column column2">
+                  <div className="-footerlogo_sec">
+                    <p>
+                         {item.footerMenuTitle}
+                    </p>
+                    <ul className="menu-list">
+                    {item.footerMenus && item.footerMenus.map((itemmenu: any, index: number) => {
+                          return (
+                            <>
+                               <li>
+                                <a
+                                  href= {itemmenu.link}
+                                  title="Do Son">
+                                  {itemmenu.label}
+                                  </a>
+                              </li>
+                            </>
+                          )
+                    })}
+                     
+                    </ul>
+                  </div>
+                </div>
+                  
+                </> 
+          )
+})}
+                <div className="column column4">
+                  <div className="em-client__colophon">
+                  {props?.footerData?.c_footerBottomContent?.map((data: any) => {
+                        return (
                           <>
-                              <p>{data}</p>
+                            <p>{data}</p>
                           </>
-                        )
-                      
+                        );
                       })}
-                       
+                  </div>
+                </div>
               </div>
+            </div>
+           <div className="copyright-footer">
               <div className="em-copyright">
-                   { props.footerData?.c_copyrightText} 
+                {props.footerData?.c_copyrightText}
               </div>
-               {/* footer Content */}
-            {/* Crawled Footer From Live Site */}
+              <div className="payment-options">
+              {props?.footerData?.c_footerPayment.textPayment ? (
+                    <>
+                         <span>{props?.footerData?.c_footerPayment.textPayment}</span>
+                    </>
+              ) : (<></>)
+            }
+              {props?.footerData?.c_footerPayment.paymentImage?.url ? (
+                    <>
+                        <img src={props?.footerData?.c_footerPayment.paymentImage?.url}></img>
+                       
+                    </>
+                  ) : (
+                    <>
+                        {svgIcons.paymentimg}
+                    </>
+                    )}
+
+              </div>
+           </div>
           </div>
         </div>
       </footer>
